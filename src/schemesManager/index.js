c950226de75ef5357e94f2d565e136d658e8cc20
John Gill <johnagill95@gmail.com>
2017-09-01 11:14:52 +0100

    save

---
+++ src/schemesManager/index.js
@@ -0,0 +1,19 @@
+import executeCommand from '~/common/executeCommand'
+import addNpmScript from '~/common/addNpmScript'
+import updateJson from '~/common/updateJson'
+
+export default () => {
+  executeCommand('npm install --save-dev react-native-schemes-manager')
+
+  addNpmScript('postinstall', 'react-native-schemes-manager all && (cd ios && pod install --verbose) && yarn clean:babelrc')
+
+  updateJson(
+    {
+      xcodeSchemes: {
+        Release: ['Staging'],
+        Debug: [],
+      },
+    },
+    './package.json',
+  )
+}
