fa9a2bfc39819bed1dc37a7aee99ee4aa1081bcc
Richard Gill <richard@rgill.co.uk>
2017-09-01 13:31:14 +0100

    made project name dynamic

--- src/reactNativeConfig/index.js
+++ src/reactNativeConfig/index.js
@@ -2,15 +2,18 @@ import { replaceInFile } from '~/common/replace'
 import executeCommand from '~/common/executeCommand'
 import addNpmScript from '~/common/addNpmScript'
 import updateJson from '~/common/updateJson'
+import { findReactNativeXcodeProjectName } from './reactNative'
 
 import { addPreProcessorEnvironments, copyBuildConfiguration } from './xcodeProject'
 
 export default () => {
   console.log('Adding React Native Config')
 
-  copyBuildConfiguration('ios/Test1.xcodeproj/project.pbxproj', 'Release', 'Staging')
+  const xcodeProjectName = findReactNativeXcodeProjectName()
 
-  addPreProcessorEnvironments('ios/Test1.xcodeproj/project.pbxproj')
+  copyBuildConfiguration(`ios/${xcodeProjectName}.xcodeproj/project.pbxproj`, 'Release', 'Staging')
+
+  addPreProcessorEnvironments(`ios/${xcodeProjectName}.xcodeproj/project.pbxproj`)
 
   executeCommand('npm install --save-dev react-native-schemes-manager')
 
@@ -30,7 +33,7 @@ export default () => {
   executeCommand('rm package-lock.json && npm install')
 
   replaceInFile({
-    files: './ios/Test1/AppDelegate.m',
+    files: `./ios/${xcodeProjectName}/AppDelegate.m`,
     from: 'initialProperties:nil',
     to: 'initialProperties:@{@"environment" : ENVIRONMENT}',
   })
---
+++ src/reactNativeConfig/reactNative.js
@@ -0,0 +1,10 @@
+import fs from 'fs'
+import _ from 'lodash'
+
+const XCODE_PROJECT_SUFFIX = '.xcodeproj'
+
+export const findReactNativeXcodeProjectName = () => {
+  const files = fs.readdirSync('./ios')
+  const xcodeProjectFile = _.find(files, f => f.endsWith(XCODE_PROJECT_SUFFIX))
+  return xcodeProjectFile.replace(XCODE_PROJECT_SUFFIX, '')
+}
