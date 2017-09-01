8338e0757a157535f0d3389302a4d2169094b9af
Richard Gill <richard@rgill.co.uk>
2017-09-01 11:27:20 +0100

    Added user defined ENV vars

--- src/reactNativeConfig/index.js
+++ src/reactNativeConfig/index.js
@@ -1,7 +1,9 @@
-import { copyBuildConfiguration } from './xcodeProject'
+import { addUserDefinedEnvironment, copyBuildConfiguration } from './xcodeProject'
 
 export default () => {
   console.log('Adding React Native Config')
 
   copyBuildConfiguration('ios/Test.xcodeproj/project.pbxproj', 'Release', 'Staging')
+
+  addUserDefinedEnvironment('ios/Test.xcodeproj/project.pbxproj')
 }
--- src/reactNativeConfig/xcodeProject.js
+++ src/reactNativeConfig/xcodeProject.js
@@ -2,8 +2,34 @@ import xcode from 'xcode'
 import _ from 'lodash'
 import fs from 'fs'
 
+const xcodeProjectFromFile = projectPath => xcode.project(projectPath).parseSync()
+
+const saveXcodeProject = (projectPath, xcodeProject) => fs.writeFileSync(projectPath, xcodeProject.writeSync())
+
+const getBuildConfigurations = projectPath => {
+  const xcodeProject = xcodeProjectFromFile(projectPath)
+  return _.chain(xcodeProject.pbxXCBuildConfigurationSection())
+    .map(buildConfiguration => buildConfiguration.name)
+    .uniq()
+    .compact()
+    .value()
+}
+
+export const addUserDefinedEnvironment = projectPath => {
+  console.log(`\nAdding user defined ENVIRONMENT variables in ${projectPath}\n`)
+
+  const xcodeProject = xcodeProjectFromFile(projectPath)
+  const buildConfigurations = getBuildConfigurations(projectPath)
+  _.forEach(buildConfigurations, buildConfiguration => {
+    const projectBuildConfig = _.find(xcodeProject.pbxXCBuildConfigurationSection(), x => !_.has(x, 'buildSettings.PRODUCT_NAME') && x.name === buildConfiguration)
+    projectBuildConfig.buildSettings.ENVIRONMENT = _.toUpper(buildConfiguration)
+  })
+  saveXcodeProject(projectPath, xcodeProject)
+}
+
 export const copyBuildConfiguration = (projectPath, existingBuildConfigName, newBuildConfigurationName) => {
-  const xcodeProject = xcode.project(projectPath).parseSync()
+  console.log(`\nCopying build configuration: ${existingBuildConfigName} to ${newBuildConfigurationName} in ${projectPath}\n`)
+  const xcodeProject = xcodeProjectFromFile(projectPath)
 
   const buildConfigurationsToCopy = _.pickBy(xcodeProject.pbxXCBuildConfigurationSection(), buildConfiguration => buildConfiguration.name === existingBuildConfigName)
 
@@ -18,5 +44,5 @@ export const copyBuildConfiguration = (projectPath, existingBuildConfigName, new
     xcodeProject.pbxXCConfigurationList()[xcConfigListKey].buildConfigurations.push({ value: newBuildConfigUuid, comment: newBuildConfigurationName })
   })
 
-  fs.writeFileSync(projectPath, xcodeProject.writeSync())
+  saveXcodeProject(projectPath, xcodeProject)
 }
