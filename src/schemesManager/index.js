90eb091347d6cc754d3f84c64517546139e37011
John Gill <johnagill95@gmail.com>
2017-09-01 12:25:17 +0100

    save

--- src/schemesManager/index.js
+++ src/schemesManager/index.js
@@ -1,6 +1,7 @@
 import executeCommand from '~/common/executeCommand'
 import addNpmScript from '~/common/addNpmScript'
 import updateJson from '~/common/updateJson'
+import replace from 'replace-in-file'
 
 export default () => {
   executeCommand('npm install --save-dev react-native-schemes-manager')
@@ -16,4 +17,12 @@ export default () => {
     },
     './package.json',
   )
+
+  const options = {
+    files: './ios/test/AppDelegate.m',
+    from: 'initialProperties:nil',
+    to: 'initialProperties:{@"environment" : ENVIRONMENT}',
+  }
+
+  replace(options)
 }
