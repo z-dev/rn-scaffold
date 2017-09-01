b4eca4b2c221d238b9d5d16990cc6cbdb1a3f4f1
John Gill <johnagill95@gmail.com>
2017-09-01 11:18:02 +0100

    save

--- src/schemesManager/index.js
+++ src/schemesManager/index.js
@@ -5,7 +5,7 @@ import updateJson from '~/common/updateJson'
 export default () => {
   executeCommand('npm install --save-dev react-native-schemes-manager')
 
-  addNpmScript('postinstall', 'react-native-schemes-manager all && (cd ios && pod install --verbose) && yarn clean:babelrc')
+  addNpmScript('postinstall', 'react-native-schemes-manager all && (cd ios && pod install --verbose)')
 
   updateJson(
     {
