bfc1e396998f5ddcc3eab777453404535482d7dd
John Gill <johnagill95@gmail.com>
2017-09-05 11:21:42 +0100

    save

--- src/provisioningProfiles/index.js
+++ src/provisioningProfiles/index.js
@@ -1,12 +1,19 @@
-import executeCommand from '~/common/executeCommand'
+import executeCommand, { checkCommandsExist } from '~/common/executeCommand'
 import prompt from 'prompt-promise'
 
 export default async () => {
+  console.log('Checking prerequisite installs')
+  checkCommandsExist(['fastlane', 'xcode-select'])
+
   console.log('Please go to https://developer.apple.com/account and access Certificates, Identifiers & Profiles')
   console.log('Under Identifiers add a new App IDs add you app, your Bundle Identifier can be found in xcode')
   console.log('Add a IOS device to the Devices tab in Certificates, Identifiers & Profiles, you device UUID can be found in xcode under window > devices when it is plugged in')
 
-  await prompt('Have you completed the previously logged steps?')
+  const response = await prompt('Have you completed the previously logged steps? (y/n)')
+
+  if (response !== 'y') {
+    process.exit(1)
+  }
 
   executeCommand(`fastlane match init`)
 
